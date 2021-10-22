5dd088f5522670d6815a5b476df49fb923d96430
Karanraj <karanrajmokan@gmail.com>
2021-10-22 11:18:08 +0530

    Committing recommendation

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route("/api")
 def index():
-    todos = "Hello bitches"
-    return Response(todos, mimetype="application/json", status=200)
+    message = "Hola"
+    return Response(message, mimetype="application/json", status=200)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
